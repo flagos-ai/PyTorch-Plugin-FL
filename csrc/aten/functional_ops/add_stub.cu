c46ca8d0727269194a4df630dafb78c70168ba8f
Hchnr <hechenrui123@gmail.com>
2026-05-08 20:40:15 +0800

    refactor: google code style formating

--- csrc/aten/functional_ops/add_stub.cu
+++ csrc/aten/functional_ops/add_stub.cu
@@ -16,32 +16,32 @@ namespace {
 
 // CUDA functors for add operation
 template <typename scalar_t>
-struct CUDAFunctorOnSelf_add {
+struct AddCudaFunctorOnSelf {
   using opmath_t = at::opmath_type<scalar_t>;
   opmath_t other_;
   opmath_t alpha_;
-  CUDAFunctorOnSelf_add(opmath_t other, opmath_t alpha) : other_(other), alpha_(alpha) {}
+  AddCudaFunctorOnSelf(opmath_t other, opmath_t alpha) : other_(other), alpha_(alpha) {}
   __device__ scalar_t operator()(scalar_t self) const {
     return static_cast<opmath_t>(self) + alpha_ * other_;
   }
 };
 
 template <typename scalar_t>
-struct CUDAFunctorOnOther_add {
+struct AddCudaFunctorOnOther {
   using opmath_t = at::opmath_type<scalar_t>;
   opmath_t self_;
   opmath_t alpha_;
-  CUDAFunctorOnOther_add(opmath_t self, opmath_t alpha) : self_(self), alpha_(alpha) {}
+  AddCudaFunctorOnOther(opmath_t self, opmath_t alpha) : self_(self), alpha_(alpha) {}
   __device__ scalar_t operator()(scalar_t other) const {
     return self_ + alpha_ * static_cast<opmath_t>(other);
   }
 };
 
 template <typename scalar_t>
-struct CUDAFunctor_add {
+struct AddCudaFunctor {
   using opmath_t = at::opmath_type<scalar_t>;
   opmath_t alpha_;
-  CUDAFunctor_add(opmath_t alpha) : alpha_(alpha) {}
+  AddCudaFunctor(opmath_t alpha) : alpha_(alpha) {}
   __device__ scalar_t operator()(scalar_t self, scalar_t other) const {
     return static_cast<opmath_t>(self) + alpha_ * static_cast<opmath_t>(other);
   }
@@ -67,17 +67,17 @@ at::Tensor add_kernel_cuda(
     [&]() {
       using opmath_t = at::opmath_type<scalar_t>;
       if (iter.is_cpu_scalar(1)) {
-        CUDAFunctorOnOther_add<scalar_t> ufunctor(
+        AddCudaFunctorOnOther<scalar_t> ufunctor(
           iter.scalar_value<opmath_t>(1), alpha.to<opmath_t>());
         iter.remove_operand(1);
         at::native::gpu_kernel(iter, ufunctor);
       } else if (iter.is_cpu_scalar(2)) {
-        CUDAFunctorOnSelf_add<scalar_t> ufunctor(
+        AddCudaFunctorOnSelf<scalar_t> ufunctor(
           iter.scalar_value<opmath_t>(2), alpha.to<opmath_t>());
         iter.remove_operand(2);
         at::native::gpu_kernel(iter, ufunctor);
       } else {
-        at::native::gpu_kernel(iter, CUDAFunctor_add<scalar_t>(alpha.to<opmath_t>()));
+        at::native::gpu_kernel(iter, AddCudaFunctor<scalar_t>(alpha.to<opmath_t>()));
       }
     }
   );
